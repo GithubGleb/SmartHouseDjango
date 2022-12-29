651919c5ec617b4e47485aebb6dea7b8351031d8
Gleb <46938gt@rambler.ru>
2022-12-29 20:21:19 +0300

    Aded options and urls, views

--- SmartHouse/SmartHouse/urls.py
+++ SmartHouse/SmartHouse/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import index, profile, productinfo
 
 urlpatterns = [
+    path('index/', index),
     path('admin/', admin.site.urls),
+    path('profile/', profile),
+    path('productinfo', productinfo)
 ]
--- SmartHouse/core/views.py
+++ SmartHouse/core/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('ok')
+
+def profile(request):
+    return HttpResponseRedirect('https://ya.ru/')
+
+def productinfo(request):
+    return HttpResponse(content = b'Item list', status = 404)
